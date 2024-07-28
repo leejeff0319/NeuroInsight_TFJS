450e7ccecca28c79c73889888defc9693618f702
leejeff0319 <leejeff0319@gmail.com>
2024-07-28 02:04:59 -0400

    added and scaled all imaged on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,14 +7,22 @@ export default function Home() {
 
       {/* Picture Banner */}
       <div className="flex h-160 w-full flex-col items-center justify-between mb-16">
-        img
+        <Image className="image-container"
+          src="/550.png" 
+          width={2600}
+          height={800}
+          alt="Main Image of website"
+        />
       </div>
 
       {/* Brief Description/ Mission Statement */}
       <div className=" flex h-52 w-full flex-col bg-white pt-20">
-        <div className="text-xl text-box ml-5 mr-5 mb-5">
-          textbox
-        </div>
+        <p className="text-xl text-box ml-5 mr-5 mb-5">
+          We are data scientists propelled by passion and personal experience.  Brain Tumors often present with unknown etiology and generic symptoms, 
+          making them difficult to detect until it is too late. 
+          NeuroQ is our personal project, developed with the hope of preventing more cases of undetected brain tumors. 
+          This model is only for reference, and should NOT be used for diagnosis.
+        </p>
       </div>
 
       {/* Explanation of Brain Tumor in today's society & Picture */}
@@ -25,13 +34,18 @@ export default function Home() {
             <b>The Elusiveness of Brain Tumor</b>
           </h2>
           <p className="text-box ml-14 mt-3" >
-            jkl
+            Statistics
           </p>
         </div>
           
         {/* Representative Image */}
         <div className="col-span-2">
-          IMAGE
+          <Image className="image-container"
+            src="/brain-tumor-300.png" 
+            width={1300}
+            height={260}
+            alt="Main Image of website"
+            />
         </div>
       </div>
 
@@ -39,18 +53,39 @@ export default function Home() {
       <div className="w-full h-60 grid text-center bg-white lg:mb-0 lg:w-full lg:grid-cols-12 lg:text-left">
 
         {/* TensorFlow Pretrained Model */}
-        <div className="col-span-4 p-2">
-          IMAGE
+        <div className="col-span-4 p-2 ">
+          <Link href="/">
+            <Image className="image-container"
+              src="/tf2-3.6125.png" 
+              width={800}
+              height={260}
+              alt="Main Image of website"
+            />
+          </Link>
         </div>
 
         {/* CNN Model from scratch */}
         <div className="col-span-4 p-2">
-          IMAGE
+        <Link href="/">
+            <Image className="image-container"
+              src="/cnn.png" 
+              width={800}
+              height={260}
+              alt="Main Image of website"
+            />
+          </Link>
         </div>
 
         {/* Last CNN Model */}
         <div className="col-span-4 p-2">
-          IMAGE
+        <Link href="/">
+            <Image className="image-container"
+              src="/fastai.png" 
+              width={800}
+              height={260}
+              alt="Main Image of website"
+            />
+          </Link>
         </div>
       </div>
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import Image from 'next/image';
+import localFont from '@next/font/local'
+
+// const myFont = localFont({ src: '/fonts/VersatyloRounded/Web-PS/VersatyloRounded.woff2' });
 
 const Navbar = () => {
+    
     return (
         <nav className="navbar">
             <div className="navbar-container pr-40">
-                <Link href="/"className="text-box pl-16">NeuroInsight</Link>
-                <div className="navbar-links text-box">
+                <Link href="/"className="text-box pl-16">
+                    <Image
+                    src="/brain_logo.jpg" 
+                    width={100}
+                    height={50}
+                    alt="logo of the website"
+                    />
+                    <div className="logo-text ml-4">
+                        NeuroQ
+                    </div>
+                </Link>
+                <div className="navbar-links text-box text-lg">
                     <Link href="/about">About</Link>
                     <Link href="/approach">Our Approach</Link>
                     <Link href="/code">Code</Link>
